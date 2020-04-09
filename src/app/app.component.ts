629495c2be2ce53a5dccac2b53976680a341a89c
Patryk <bula00721@gmail.com>
2020-04-09 19:22:32 +0200

    Sekcja 37

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularUdemyNauka';
+  newTask: string;
+  taskList: Array<TaskClass> = [];
+  taskDone: Array<TaskClass> = [];
+
+  add() {
+    this.taskList.push(new TaskClass(this.newTask));
+    this.newTask = '';
+    console.log(this.taskList);
+  }
+  remove(task) {
+    this.taskList = this.taskList.filter(e => e !== task);
+  }
+
+  done(task) {
+    this.taskDone.push(task);
+    this.remove(task);
+  }
+}
+
+class TaskClass {
+  private readonly value: string;
+  constructor(value: string) {
+    this.value = value;
+  }
+  getValue() {
+    return this.value;
+  }
 }
